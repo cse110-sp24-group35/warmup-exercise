08f05f6f48c22bfe72e2be222288565936efd7de
nade <i@nade.me>
2024-05-12 21:40:18 -0700

    Add test to subtract

--- test/unit/dummy.test.js
+++ test/unit/dummy.test.js
@@ -1,4 +1,4 @@
-const { sum } = require('../../dummy.js');
+const { sum, subtract } = require('../../dummy.js');
 const { assert, expect } = require('chai');
 
 describe("sum(a, b)", () => {
@@ -14,5 +14,19 @@ describe("sum(a, b)", () => {
         let b = 2;
 
         expect(sum(a, b)).to.equal(4);
+    });
+
+    it("subtract(a, b) calculated properly", () => {
+        let a = 2;
+        let b = 2;
+
+        expect(subtract(a, b)).to.equal(0);
+    });
+
+    it("subtract(a, b) returns right type", () => {
+        let a = 1;
+        let b = 1;
+
+        assert.isNumber(subtract(a, b));
     });
 });
